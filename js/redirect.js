e76c61d1309e6d25a3dc20ffbc1ab4a4bc258697
Kevin Romanello <kevin.romanello@studenti.unitn.it>
2025-04-14 22:11:58 +0200

    piccole modifiche ai link pt 3

---
+++ js/redirect.js
@@ -0,0 +1,35 @@
+function GoToDolomiti() {
+    const elemento = document.getElementById("Dolomiti");
+    if (elemento) {
+        elemento.scrollIntoView({ 
+            behavior: 'smooth' // Fa in modo che lo scorrimento sia fluido
+        });
+    }
+}
+
+function GoToBossi() {
+    const elemento = document.getElementById("Bossiagenzie");
+    if (elemento) {
+        elemento.scrollIntoView({ 
+            behavior: 'smooth' // Fa in modo che lo scorrimento sia fluido
+        });
+    }
+}
+
+function GoTo3d() {
+    const elemento = document.getElementById("3dSito");
+    if (elemento) {
+        elemento.scrollIntoView({ 
+            behavior: 'smooth' // Fa in modo che lo scorrimento sia fluido
+        });
+    }
+}
+
+function GoToAltri() {
+    const elemento = document.getElementById("Altri");
+    if (elemento) {
+        elemento.scrollIntoView({ 
+            behavior: 'smooth' // Fa in modo che lo scorrimento sia fluido
+        });
+    }
+}
